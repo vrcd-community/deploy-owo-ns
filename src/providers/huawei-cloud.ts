be280aec8a31b6c59bd6a58199b8355a40e999ce
Misaka-L <lipww1234@foxmail.com>
2024-12-13 23:49:07 +0800

    feat(provider): huawei-cloud provider

---
+++ src/providers/huawei-cloud.ts
@@ -0,0 +1,163 @@
+import { DnsProvider } from '../types/dns-provider.ts'
+import { DnsRecord, DnsRecordDto, DnsRecordType } from '../types/dns-record.ts'
+
+import { BasicCredentials } from '@huaweicloud/huaweicloud-sdk-core'
+import {
+  BatchCreateRecordSetWithLine,
+  CreateRecordSetWithBatchLinesRequest,
+  CreateRSetBatchLinesReq,
+  DeleteRecordSetsRequest,
+  DnsClient,
+  ListPublicZonesRequest,
+  ShowRecordSetByZoneRequest,
+  ShowRecordSetByZoneResp,
+} from '@huaweicloud/huaweicloud-sdk-dns'
+
+export default class HuaweiCloudDnsProvider implements DnsProvider {
+  readonly id: string = 'tencent-cloud'
+  readonly name: string = 'Tencent Cloud (DnsPod)'
+
+  private readonly client: DnsClient
+
+  private readonly ispMap: Record<string, string> = {
+    '中国电信': 'Dianxin',
+    '中国联通': 'Liantong',
+    '中国移动': 'Yidong',
+  }
+
+  constructor(env: Record<string, string>) {
+    const credentials = new BasicCredentials()
+      .withAk(env.HUAWEICLOUD_SDK_AK)
+      .withSk(env.HUAWEICLOUD_SDK_SK)
+
+    this.client = DnsClient.newBuilder()
+      .withCredential(credentials)
+      .withEndpoint(env.HUAWEICLOUD_SDK_ENDPOINT)
+      .build()
+  }
+
+  async addRecords(domain: string, records: DnsRecord[]): Promise<void> {
+    const zoneId = await this.getZoneId(domain)
+
+    const recordGroup = records.reduce((acc, record) => {
+      const key = `${record.name}-${record.type}`
+      acc[key] = acc[key] ?? []
+      acc[key].push(record)
+      return acc
+    }, {} as Record<string, DnsRecord[]>)
+
+    const recordSets: CreateRSetBatchLinesReq[] = []
+
+    for (const key in recordGroup) {
+      const records = recordGroup[key]
+
+      const recordSet = new CreateRSetBatchLinesReq()
+        .withName(records[0].name + '.' + domain + '.')
+        .withType(records[0].type)
+        .withLines(records.map((record) => {
+          return new BatchCreateRecordSetWithLine()
+            .withLine(this.ispMap[record.line] ?? record.line)
+            .withTtl(record.ttl ?? 300)
+            .withRecords([record.value])
+        }))
+
+      recordSets.push(recordSet)
+    }
+
+    for (const recordSet of recordSets) {
+      await this.client.createRecordSetWithBatchLines(
+        new CreateRecordSetWithBatchLinesRequest()
+          .withZoneId(zoneId)
+          .withBody(recordSet),
+      )
+    }
+  }
+
+  async removeRecords(domain: string, recordIds: string[]): Promise<void> {
+    const zoneId = await this.getZoneId(domain)
+
+    // send a http DELETE request with body will crash the Deno node-http compatibility layer
+    // https://github.com/denoland/deno/issues/22565
+    // https://console-intl.huaweicloud.com/apiexplorer/#/openapi/DNS/debug?api=BatchDeleteRecordSetWithLine
+    // so DON'T use follow code util Deno fix this issue or Huawei Cloud change their API
+
+    // await this.client.batchDeleteRecordSetWithLine(
+    //   new BatchDeleteRecordSetWithLineRequest()
+    //     .withZoneId(zoneId)
+    //     .withBody(new BatchDeleteRecordSetWithLineRequestBody()
+    //       .withRecordsetIds(recordIds)),
+    // )
+
+    for (const recordId of recordIds) {
+      await this.client.deleteRecordSets(
+        new DeleteRecordSetsRequest()
+          .withZoneId(zoneId)
+          .withRecordsetId(recordId),
+      )
+    }
+  }
+
+  async getRecords(
+    domain: string,
+    type?: DnsRecordType,
+    line?: string,
+    name?: string,
+  ): Promise<DnsRecordDto[]> {
+    const zoneId = await this.getZoneId(domain)
+
+    const showRequest = new ShowRecordSetByZoneRequest()
+      .withZoneId(zoneId)
+      .withLimit(500)
+
+    showRequest.type = type
+    showRequest.name = name
+    showRequest.lineId = line
+
+    let records: ShowRecordSetByZoneResp[] = []
+    const initialRecords = await this.client.showRecordSetByZone(showRequest)
+    const pageToFetch = Math.ceil(
+      initialRecords.metadata?.totalCount! / showRequest.limit!,
+    )
+
+    records = records.concat(initialRecords.recordsets!)
+
+    for (let index = 1; index < pageToFetch; index++) {
+      const response = await this.client.showRecordSetByZone(
+        showRequest.withOffset(index * showRequest.limit!),
+      )
+
+      records = records.concat(response.recordsets!)
+    }
+
+    return records.map((record) => this.toDnsRecord(record, domain))
+  }
+
+  private toDnsRecord(
+    record: ShowRecordSetByZoneResp,
+    domain: string,
+  ): DnsRecordDto {
+    const line = this.ispMap[record.line!] ?? record.line!
+
+    return {
+      id: record.id!,
+      name: record.name!.replace(`.${domain}.`, ''),
+      type: record.type!,
+      ttl: record.ttl!,
+      line: line,
+      value: record.records![0],
+    }
+  }
+
+  private async getZoneId(domain: string): Promise<string> {
+    const zones = await this.client.listPublicZones(
+      new ListPublicZonesRequest().withName(domain),
+    )
+    const zone = zones.zones?.find((zone) => zone.name === domain + '.')
+
+    if (!zone || !zone.id) {
+      throw new Error(`Zone not found: ${domain}`)
+    }
+
+    return zone.id
+  }
+}
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,13 +1,15 @@
 import z from 'zod'
 
 import { DnsProviderConstructor } from '../types/dns-provider.ts'
+import HuaweiCloudDnsProvider from './huawei-cloud.ts'
 import TencentCloudDnsProvider from './tencent-cloud.ts'
 
 export default {
   'tencent-cloud': TencentCloudDnsProvider,
+  'huawei-cloud': HuaweiCloudDnsProvider,
 } as Record<DnsProviderKeys, DnsProviderConstructor | undefined>
 
-export const DnsProviderKeys = z.enum(['tencent-cloud'], {
+export const DnsProviderKeys = z.enum(['tencent-cloud', 'huawei-cloud'], {
   message: 'Unsupported or invalid DNS provider',
 })
 export type DnsProviderKeys = z.infer<typeof DnsProviderKeys>
